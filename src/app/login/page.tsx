169e7f4066d06e7bdc142567bf31269f62888fd9
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-05-28 14:34:04 +0530

    boiler plate code done

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Login = () => {
+    return (
+        <section className="flex justify-center items-center min-h-screen">
+            <div className=" border-2 border-white/40"></div>
+        </section>
+    );
+};
+
+export default Login;
